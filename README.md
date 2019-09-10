dedfff17961d3e8bbcfe851b7c83f0f4b573cf96
Goutam Shepur <goutam.s.h@gmail.com>
2019-09-10 23:05:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+React app created with below command
+
+```
+npx create-react-app harmonica-ear-training-web
+cd harmonica-ear-training-web
+npm start
+```
+
---
+++ harmonica-ear-training-backend/src/main/java/com/gshepur/harp/HarmonicaEarTrainingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gshepur.harp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonicaEarTrainingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonicaEarTrainingBackendApplication.class, args);
+	}
+
+}
