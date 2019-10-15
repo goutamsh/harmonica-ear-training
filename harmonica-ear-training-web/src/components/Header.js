86b5b6f1e8cb6188aab2971d8ab3285e4574298c
Goutam Shepur <goutam.s.h@gmail.com>
2019-10-15 22:17:54 +0100

    HArmonica login page

--- harmonica-ear-training-web/src/components/Header.js
+++ harmonica-ear-training-web/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component{
 
     render(){
         return(
             <Jumbotron >
-                {localStorage.getItem('user') ? <h6>Hi User</h6> : <h6>LogIn123</h6>}
+                {localStorage.getItem('user') ? <h6>Hi User</h6> : <h6>Hi Guest <Link to="/login">Login</Link></h6>}
                 <center><h1>Harmonica Ear Training</h1></center>
             </Jumbotron>        
         );
